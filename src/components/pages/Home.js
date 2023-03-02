36cbdd08e6a8ed1864a322d08d6406f4dfc95c19
D <dickerson.morgan@outlook.com>
2023-03-01 21:15:29 -0500

    added images to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Container } from '@mui/system';
-import {Box, Card, CardContent, Typography} from '@mui/material';
+import { Box, Card, CardContent, Typography } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Image from "../../images/nerdyHeart.png"
 import "../../styles/Home.css"
@@ -20,8 +20,22 @@ export default function Home() {
                         src={Image}
                     />
                 </Grid2>
-                <Grid2 xs={12} md={7} className="test">
-                    <Card sx={{ minWidth: 275, fontFamily: 'Quicksand' }}>
+                <Grid2 xs={12} md={7}>
+                    <Card sx={{ minWidth: 275 }}>
+                        <CardContent>
+                            <div className='homeImage'>
+                                <img src="https://64.media.tumblr.com/12c8aba66bffc7d96ab54cbce763c1f9/9cef6d1425b04a53-8a/s400x600/32c9774d5e931ace856d47772b5fef39d6cfbb3f.gifv" alt="rainbow bouncy star gif" /><img src="https://64.media.tumblr.com/12c8aba66bffc7d96ab54cbce763c1f9/9cef6d1425b04a53-8a/s400x600/32c9774d5e931ace856d47772b5fef39d6cfbb3f.gifv" alt="rainbow bouncy star gif" />
+                            </div>
+                            <Typography variant="h4">
+                                Welcome, Starlight
+                            </Typography>
+                            <Typography variant="body1">
+                                I'll put proper words here eventually
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <br></br>
+                    <Card sx={{ minWidth: 275 }}>
                         <CardContent>
                             <Typography variant="h4">
                                 Welcome!
